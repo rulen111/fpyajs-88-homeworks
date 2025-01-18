65e465954bae98374c42fdb2802146954de4c2a0
“Ruslan <akhmarovri@yandex.ru>
2025-01-18 15:48:23 +0300

    HW 2 Task 2 Done

--- modules/src/js/app.js
+++ modules/src/js/app.js
@@ -4,3 +4,9 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+// Task 2 Code
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game'
+
+const game = new Game();
+game.start();
